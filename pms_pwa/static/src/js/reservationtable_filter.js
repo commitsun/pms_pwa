f8b2f7bca7e13db3a55d7db8e4c805c39f58f891
Manuel Alejandro Núñez Liz <alejandro@zeleiro.com>
2020-08-12 13:52:07 +0200

    [ADD] front-end files

---
+++ pms_pwa/static/src/js/reservationtable_filter.js
@@ -0,0 +1,19 @@
+function searchingFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("reservationListTable");
+    tr = table.getElementsByTagName("tr");
+    
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
