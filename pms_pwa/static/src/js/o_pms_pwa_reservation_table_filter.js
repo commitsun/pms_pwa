946c18c025fa129d56edba20998069894f36c494
vicentecom <vicente@comunitea.com>
2021-03-16 11:48:48 +0100

    [IMP] pms_pwa: Check in selector on modal. List record now reloads correctly

--- pms_pwa/static/src/js/o_pms_pwa_reservation_table_filter.js
+++ pms_pwa/static/src/js/o_pms_pwa_reservation_table_filter.js
@@ -205,11 +205,8 @@ odoo.define("pms_pwa.reservation_table", function (require) {
             });
             alert_div.append(alert);
             $(String("#reservation_" + data_id)).load(
-                String(window.location.href + " #reservation_" + data_id)
+                String(window.location.href + " #reservation_" + data_id + " td")
             );
-            /* $("#o_pms_pwa_reservation_list_table").load(
-                window.location.href + " #o_pms_pwa_reservation_list_table"
-            ) */
         },
         /* OnClick events */
         _onClickReservationButton: function (event) {
@@ -376,6 +373,67 @@ odoo.define("pms_pwa.reservation_table", function (require) {
                                         new_data,
                                         reservation_data.id
                                     );
+                                }
+                            );
+                        });
+
+                        // DATE RANGE MODAL
+                        $(function () {
+                            $('input[name="range_check_date_modal"]').daterangepicker(
+                                {
+                                    locale: {
+                                        direction: "ltr",
+                                        format: "DD/MM/YYYY",
+                                        separator: " - ",
+                                        applyLabel: "Aplicar",
+                                        cancelLabel: "Cancelar",
+                                        fromLabel: "Desde",
+                                        toLabel: "hasta",
+                                        customRangeLabel: "Custom",
+                                        daysOfWeek: [
+                                            "Do",
+                                            "Lu",
+                                            "Ma",
+                                            "Mi",
+                                            "Ju",
+                                            "Vi",
+                                            "Sa",
+                                        ],
+                                        monthNames: [
+                                            "Enero",
+                                            "Febrero",
+                                            "Marzo",
+                                            "Abril",
+                                            "Mayo",
+                                            "Junio",
+                                            "Julio",
+                                            "Agosto",
+                                            "Septiembre",
+                                            "Octubre",
+                                            "Noviembre",
+                                            "Diciembre",
+                                        ],
+                                        firstDay: 1,
+                                    },
+
+                                    opens: "left",
+                                    showCustomRangeLabel: false,
+                                },
+                                function (start, end, label) {
+                                    console.log(label);
+                                    $('input[name="check_in_date"]').val(start);
+                                    $('input[name="check_out_date"]').val(end);
+                                    let nights = 1;
+                                    // Hours*minutes*seconds*milliseconds
+                                    const oneDay = 24 * 60 * 60 * 1000;
+                                    const firstDate = new Date(start);
+                                    const secondDate = new Date(end);
+                                    const diffDays = Math.round(
+                                        Math.abs((firstDate - secondDate) / oneDay)
+                                    );
+                                    nights = diffDays - 1;
+                                    $('input[name="nights"]').val(nights);
+                                    // $("form#reservation_detail").submit();
                                 }
                             );
                         });
