8b00b068ebe4cf0e5dbd13a6db5f2280629733bf
Manuel Alejandro Núñez Liz <lordchucu@gmail.com>
2021-05-04 09:11:22 +0200

    [ADD] calendar config mods

--- pms_pwa/static/src/js/o_pms_pwa_calendar config.js
+++ pms_pwa/static/src/js/o_pms_pwa_calendar config.js
@@ -2,7 +2,6 @@ odoo.define("pms_pwa.calendar_config", function () {
     "use strict";
 
     $(document).on("focusout", "input[type='text']", function () {
-        alert("Wee");
         console.log("Salgo");
     });
 });
