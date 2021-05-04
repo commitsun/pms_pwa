4416bdbc01e7fca0d694bd9ab09ee2c1c59d37fc
Manuel Alejandro Núñez Liz <lordchucu@gmail.com>
2021-05-04 09:56:14 +0200

    [ADD] calendar config mods

--- pms_pwa/static/src/js/o_pms_pwa_calendar_config.js
+++ pms_pwa/static/src/js/o_pms_pwa_calendar_config.js
@@ -1,7 +1,7 @@
 odoo.define("pms_pwa.calendar_config", function () {
     "use strict";
 
-    $(document).on("focusout", "input[type='text']", function () {
+    $("#calendar_config_table").on("change", "input[type='text']", function () {
         this.style.backgroundColor = "yellow";
         var element = document.getElementById("save");
         element.classList.remove("d-none");
