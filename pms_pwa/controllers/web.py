48579bf00815d6607dffc51fc640f23fec840067
Dario Lodeiros <dario@commitsun.com>
2021-07-19 11:54:05 +0200

    [IMP] new reservation compute agency sale channel and alloweds

--- pms_pwa/controllers/web.py
+++ pms_pwa/controllers/web.py
@@ -74,17 +74,19 @@ class TestFrontEnd(http.Controller):
         return allowed_journals
 
     def _get_allowed_channel_type_ids(self):
-        channel_types = request.env["pms.sale.channel"].search(
-            [("is_on_line", "=", False)]
-        )
+        domain = [("is_on_line", "=", False)]
+        channel_types = request.env["pms.sale.channel"].search(domain)
         allowed_channel_types = []
         for channel in channel_types:
             allowed_channel_types.append({"id": channel.id, "name": channel.name})
         return allowed_channel_types
 
-    def _get_allowed_agency_ids(self):
+    def _get_allowed_agency_ids(self, channel_type_id=False):
+        domain = [("is_on_line", "=", False)]
+        if channel_type_id:
+            domain.append(("id", "=", channel_type_id))
         channel_types_ids = (
-            request.env["pms.sale.channel"].search([("is_on_line", "=", False)]).ids
+            request.env["pms.sale.channel"].search(domain).ids
         )
         agencies = request.env["res.partner"].search(
             [
@@ -759,7 +761,9 @@ class TestFrontEnd(http.Controller):
             "allowed_pricelists": reservation._get_allowed_pricelists(),
             "allowed_segmentations": reservation._get_allowed_segmentations(),
             "allowed_channel_type_ids": self._get_allowed_channel_type_ids(),
-            "allowed_agency_ids": self._get_allowed_agency_ids(),
+            "allowed_agency_ids": self._get_allowed_agency_ids(
+                channel_type_id = reservation.channel_type_id.id if reservation.channel_type_id else False
+            ),
             "readonly_fields": ["arrival_hour", "departure_hour"],
             "required_fields": ["room_type_id"],
         }
@@ -1731,7 +1735,9 @@ class TestFrontEnd(http.Controller):
             "allowed_pricelists": reservation._get_allowed_pricelists(),
             "allowed_segmentations": reservation._get_allowed_segmentations(),
             "allowed_channel_type_ids": self._get_allowed_channel_type_ids(),
-            "allowed_agency_ids": self._get_allowed_agency_ids(),
+            "allowed_agency_ids": self._get_allowed_agency_ids(
+                channel_type_id=reservation.channel_type_id.id if reservation.channel_type_id else False
+            ),
             "segmentation_ids": reservation.segmentation_ids.ids,
             "room_numbers": rooms.Rooms._get_available_rooms(
                 self=self,
@@ -1825,7 +1831,9 @@ class TestFrontEnd(http.Controller):
         wizard_values["channel_type_id"] = wizard.channel_type_id.id
         wizard_values["agency_id"] = wizard.agency_id.id
         wizard_values["allowed_channel_type_ids"] = self._get_allowed_channel_type_ids()
-        wizard_values["allowed_agency_ids"] = self._get_allowed_agency_ids()
+        wizard_values["allowed_agency_ids"] = self._get_allowed_agency_ids(
+            channel_type_id=wizard.channel_type_id.id if wizard.channel_type_id else False
+        )
 
         lines = {}
         for line in wizard.availability_results:
