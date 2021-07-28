90faf4d280c573813b6f691d75bef8e0caa62ec3
vicentecom <vicente@comunitea.com>
2021-07-28 12:41:31 +0200

    [IMP] pms_pwa: - Add notification views. - Fix notification display error on _onPoll.

--- pms_pwa/static/src/js/o_pms_pwa_notify.js
+++ pms_pwa/static/src/js/o_pms_pwa_notify.js
@@ -7,22 +7,32 @@ odoo.define("pms_pwa.LongpollingFront", function (require) {
     require("web.ServicesMixin");
     var Longpolling = require("bus.Longpolling");
 
+    $("a.o_pms_pwa_clear_all").on("click", function (event) {
+        event.preventDefault();
+        console.log("Reloading...");
+        location.reload();
+    });
+
     Longpolling.include({
         init: function () {
             this._super.apply(this, arguments);
-
             this._addChannelPMS();
         },
         _onPoll: function (notifications) {
+            console.log("notifications", notifications);
             this.bus_front_notification(notifications);
             this._addChannelPMS();
-            return this._super;
+            return this._super(notifications);
         },
         bus_front_notification: function (notifications) {
             var self = this;
             _.each(notifications, function (notification) {
-                var message = notification.message;
-                self.on_front_message(message);
+                if (notification.channel.startsWith("notify_pms_")) {
+                    var message = notification.message;
+                    self.on_front_message(message);
+                    self._callLocalStorage('setItem', 'last', notification.id);
+                    self._callLocalStorage('setItem', 'last_ts', new Date().getTime());
+                }
             });
         },
         _addChannelPMS: function () {
@@ -31,22 +41,33 @@ odoo.define("pms_pwa.LongpollingFront", function (require) {
             this.addChannel(channel_pms);
         },
         on_front_message: function (message) {
-            var div = document.getElementsByClassName("o_pms_pwa_notifications");
+            var div = document.getElementsByClassName("o_pms_pwa_notification_list");
+            var sup_div = document.getElementsByClassName("o_pms_pwa_notifications");
             var span = document.createElement("span");
             var content = document.createTextNode(message);
             span.appendChild(content);
             if (div && div[0]) {
                 div[0].appendChild(span);
-            }
-            if (
-                div[0].childNodes.length > 1 &&
-                div[0].attributes.class.value.includes(
-                    "o_pms_pwa_notifications_regular"
-                )
-            ) {
-                document.getElementsByClassName(
-                    "o_pms_pwa_notifications"
-                )[0].style.display = "block ruby";
+
+                if (
+                    div[0].children.length > 0 &&
+                    sup_div[0].attributes.class.value.includes(
+                        "o_pms_pwa_notifications_regular"
+                    )
+                ) {
+                    try {
+                        $(".o_pms_pwa_notification_list").css("display", "block ruby");
+                        $(".o_pms_notification_title").css("display", "unset");
+                        $(".o_pms_pwa_clear_all").css("display", "unset");
+                        $(".o_pms_pwa_update_counter").text(div[0].children.length);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+            } else {
+                if (sup_div && sup_div[0]) {
+                    sup_div[0].appendChild(span);
+                }
             }
         },
     });
