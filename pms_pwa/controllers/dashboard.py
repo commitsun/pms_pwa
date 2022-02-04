b55339542d60a6b34db4f75a7b885fe1f43c6868
Alejandro Nuñez Liz <alejandro@zeleiro.com>
2022-02-04 18:03:37 +0100

    [ADD] modals

--- pms_pwa/controllers/dashboard.py
+++ pms_pwa/controllers/dashboard.py
@@ -59,6 +59,28 @@ class DashBoard(http.Controller):
 
         values.update(
             {
+                # Cambios documento
+                "cash": {
+                    "coins": {
+                        "500": 0,
+                        "200": 0,
+                        "100": 0,
+                        "50": 0,
+                        "20": 0,
+                        "10": 0,
+                        "5": 0,
+                        "2": 0,
+                        "1": 0,
+                        "0,5": 0,
+                        "0,2": 0,
+                        "0,1": 0,
+                        "0,05": 0,
+                        "0,02": 0,
+                        "0,01": 0,
+
+                    },
+                },
+                # Fin cambio documento
                 "tasks": _get_user_activities(
                     request.session.uid if request.session.uid else False
                 ),
